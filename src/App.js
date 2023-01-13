52837e100bce33e6ee546f9c12b577367e4f0d29
Andre Jarboe <andrejarboe@gmail.com>
2023-01-13 14:57:29 -0500

    add handleSubmit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,16 @@ function App() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('hello');
+		if (!name) {
+			//display alert
+		} else if (name && isEditing) {
+			// deal with edit
+			const newItem = {
+				id: new Date().getTime().toString(),
+				title: name,
+			};
+			setName('');
+		}
 	};
 
 	return (
@@ -21,13 +30,19 @@ function App() {
 					{alert.show && <Alert />}
 					<h3>grocery bud</h3>
 					<div className="form-control">
-            <input type="text" className="grocery" placeholder="e.g. Bacon" value={name} onChange={(e) => setName(e.target.value)} />
+						<input
+							type="text"
+							className="grocery"
+							placeholder="e.g. Bacon"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
 						<button type="submit" className="submit-btn">
 							{isEditing ? 'edit' : 'submit'}
 						</button>
 					</div>
 				</form>
-			<List />
+				<List items={list} />
 				<button className="clear-btn">clear items</button>
 			</div>
 		</section>
