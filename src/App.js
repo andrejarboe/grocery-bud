26b9ae13e7cd82c9d6af8afcbe58cab9e6467f2a
Andre Jarboe <andrejarboe@gmail.com>
2023-01-13 14:52:08 -0500

    add form and state values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [name, setName] = useState('');
+	const [list, setList] = useState([]);
+	const [isEditing, setIsEditing] = useState(false);
+	const [editID, setEditID] = useState(null);
+	const [alert, setAlert] = useState({ show: false, message: '', type: '' });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hello');
+	};
+
+	return (
+		<section className="section-center">
+			<div className="grocery-container">
+				<form className="grocery-form" onSubmit={handleSubmit}>
+					{alert.show && <Alert />}
+					<h3>grocery bud</h3>
+					<div className="form-control">
+            <input type="text" className="grocery" placeholder="e.g. Bacon" value={name} onChange={(e) => setName(e.target.value)} />
+						<button type="submit" className="submit-btn">
+							{isEditing ? 'edit' : 'submit'}
+						</button>
+					</div>
+				</form>
+			<List />
+				<button className="clear-btn">clear items</button>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
